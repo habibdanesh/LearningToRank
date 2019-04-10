d434dc88432da7c450569b2bd6a7e1555a9cd2da
Habib Daneshpajouh <habib.dpajouh@gmail.com>
2019-04-09 21:42:36 -0700

    Modified RankLib files

---
+++ MetricScorer.java
@@ -0,0 +1,91 @@
+/*===============================================================================
+ * Copyright (c) 2010-2012 University of Massachusetts.  All Rights Reserved.
+ *
+ * Use of the RankLib package is subject to the terms of the software license set 
+ * forth in the LICENSE file included with this software, and also available at
+ * http://people.cs.umass.edu/~vdang/ranklib_license.html
+ *===============================================================================
+ */
+
+package ciir.umass.edu.metric;
+
+import ciir.umass.edu.learning.RankList;
+import ciir.umass.edu.utilities.Sorter;
+
+import java.util.List;
+
+/**
+ * @author vdang
+ * A generic retrieval measure computation interface. 
+ */
+public abstract class MetricScorer {
+
+	/** The depth parameter, or how deep of a ranked list to use to score the measure. */
+	protected int k = 10;
+	
+	public MetricScorer() 
+	{
+		
+	}
+
+	/**
+	 * The depth parameter, or how deep of a ranked list to use to score the measure.
+	 * @param k the new depth for this measure.
+	 */
+	public void setK(int k)
+	{
+		this.k = k;
+	}
+	/** The depth parameter, or how deep of a ranked list to use to score the measure. */
+	public int getK()
+	{
+		return k;
+	}
+	public void loadExternalRelevanceJudgment(String qrelFile)
+	{
+		
+	}
+	public double score(List<RankList> rl)
+	{
+
+		double score = 0.0;
+		double sigmaWeights= 0.0;
+		double scoresAndWeights = 0.0;
+		double weight = 0.0;
+
+		for(int i=0;i<rl.size();i++)
+		{
+			score += score(rl.get(i));
+			weight = getImportenceWeight(rl.get(i));
+			scoresAndWeights += score(rl.get(i)) * weight;
+			sigmaWeights += weight;
+		}
+		
+		return scoresAndWeights/sigmaWeights ;
+		//return score/rl.size();
+	}
+	
+	protected int[] getRelevanceLabels(RankList rl)
+	{
+		int[] rel = new int[rl.size()];
+		for(int i=0;i<rl.size();i++)
+			rel[i] = (int)rl.get(i).getLabel();
+		return rel;
+	}
+
+	// begin added by Fatemeh
+    public double getImportenceWeight(RankList rl) 
+    {
+        int[] rel = getRelevanceLabels(rl); 
+        int[] idx = Sorter.sort(rel, false); 
+        double weight = 0; 
+        for(int i=0;i<k;i++) 
+            weight += rel[idx[i]]; 
+        return weight; 
+    }
+	
+	public abstract double score(RankList rl);
+	public abstract MetricScorer copy();
+	public abstract String name();
+	public abstract double[][] swapChange(RankList rl);
+}
